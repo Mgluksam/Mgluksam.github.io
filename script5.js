fde39c263978d794c6ab2ece8c1888a57b2d4207
Mgluksam <113050727+Mgluksam@users.noreply.github.com>
2022-10-30 12:55:12 +0300

    Add files via upload

--- script5.js
+++ script5.js
@@ -1,13 +1,16 @@
-function click1() {
+window.addEventListener("DOMContentLoaded", function () {
     let n1, n2, r;
-    n1 = document.getElementById("field1").value;
-    n2 = document.getElementById("field2").value;
-    let temp = new RegExp(/^\d+$/);
-    if (temp.test(n1) && temp.test(n2)) {
-        document.getElementById("result").innerHTML = parseInt(n1) * parseInt(n2);
-        document.getElementById("result").classList.remove("warning");
-    } else {
-        document.getElementById("result").innerHTML = "Параметры заданы неверно";
-        document.getElementById("result").classList.add("warning");
-    }
-}
+    let b = document.getElementById("calculate");
+    b.addEventListener("click", function () {
+        n1 = document.getElementById("field1").value;
+        n2 = document.getElementById("field2").value;
+        let temp = new RegExp(/^\s*\d+\s*$/);
+        if (temp.test(n1) && temp.test(n2)) {
+            document.getElementById("result").innerHTML = parseInt(n1) * parseInt(n2);
+            document.getElementById("result").classList.remove("warning");
+        } else {
+            document.getElementById("result").innerHTML = "Параметры заданы неверно";
+            document.getElementById("result").classList.add("warning");
+        }
+    });
+});
