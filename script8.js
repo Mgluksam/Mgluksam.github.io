52bef056e0cb181113eb96e6b299b1e3dc71416b
Mgluksam <113050727+Mgluksam@users.noreply.github.com>
2022-11-19 21:07:55 +0300

    Add files via upload

--- script8.js
+++ script8.js
@@ -1,7 +1,58 @@
-let orderButton = document.getElementById("orderButton");
-orderButton.addEventListener('click',function(e){
-    state= {
-        page: "index8/page",
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", formSend);
+    
+    function formSend(e){
+        e.preventDefault();
+        let error = formValidate(form);
+        
     }
-    history.pushState(state, "", state.page) // Объект состояния, описание, относительная ссылка
+    function formValidate(form){ // Проверка валидации формы
+        let error = 0;
+        let formReq = document.querySelectorAll(".checkReq"); // Поля, которые необходимо проверять
+        for (let index = 0; index < formReq.length; index++){
+            const input = formReq[index];
+            formRemoveError(input);
+            if (input.id == "FormEmail"){
+                if (emailTest(input)){
+                    formAddError(input);
+                    error++;
+                }
+            }
+            else if (input.getAttribute("type")==="checkbox" && input.checked===false){
+                formAddError(input);
+                error++;
+            }
+            else{
+                if (input.value === ""){
+                    formAddError(input);
+                    error++;
+                }
+            }
+            
+        }
+        function formAddError(input){ // Добавление класса error к элементу
+            input.classList.add("error");
+        }
+        function formRemoveError(input){ // Удаление класса error к элементу
+            input.classList.remove("error");
+        }
+        function emailTest(input){ // Функция проверки корректности введенного email
+            const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+            return !EMAIL_REGEXP.test(input.value);
+        }
+        
+    }
+    
+//    let orderButton = document.getElementById("orderButton"); // History API
+//    orderButton.addEventListener('click', function (e) {
+//        state = {
+//            page: "index8.html/" + e.target.getAttribute('href'),
+//        }
+//        history.pushState(state, "", state.page) // Объект состояния, описание, относительная ссылка
+//    });
+//
+//    window.addEventListener('popstate', function (e) { // Обработчик нажатия вперед/назад пользователя
+//        document.getElementById("closeOrderButton").click();
+//    });
 });
