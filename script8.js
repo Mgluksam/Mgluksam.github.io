ff417fa109a707dfe6fa8ad3993d5c088e223f85
Mgluksam <113050727+Mgluksam@users.noreply.github.com>
2022-11-19 16:57:39 +0300

    Add files via upload

--- script8.js
+++ script8.js
@@ -1,7 +1,7 @@
 let orderButton = document.getElementById("orderButton");
 orderButton.addEventListener('click',function(e){
     state= {
-        page: "page",
+        page: "index8/page",
     }
     history.pushState(state, "", state.page) // Объект состояния, описание, относительная ссылка
 });
